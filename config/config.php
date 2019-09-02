5abaac3e83a8079d158fa7cbc300094629a01a66
hermes11 <54098574+hermes11@users.noreply.github.com>
2019-09-02 20:25:07 +0800

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php   
+		Define ('ROOT_URL', 'http://localhost/finalproject-master/finalproject-master/');
+		Define ('DB_HOST', 'localhost');
+		Define ('DB_USER', 'root');
+		Define ('DB_PASS', '');
+		Define ('DB_NAME', 'evaluation');
+
+  
+ ?>
